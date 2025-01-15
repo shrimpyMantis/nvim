0121207726b14d1757b7bcd8f0c521071d257671
Xander Rall <rallxander74@gmail.com>
2025-01-15 10:11:00 +0200

    update deprecated lsserver to ls_ts

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,7 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
-					"tsserver",
+					"ts_ls",
 					"ast_grep",
 				},
 			})
@@ -31,7 +31,7 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
+			lspconfig.ts_ls.setup({})
 			lspconfig.ast_grep.setup({})
             lspconfig.clangd.setup({})
 
